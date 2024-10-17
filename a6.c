845881bc9e747e09f3fc2bf49098c53e8f46ae8d
Rhimzy <heetlmao@outlook.com>
2024-10-17 22:41:16 +0530

    roadblock

---
+++ a6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter an integer to check if it is a prime number: ");
+    scanf("%d", &n);
+
+    // couldnt figure out a proper logic for this
+
+}
